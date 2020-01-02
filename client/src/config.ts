81273d91a4f69caf651e5bd459dcbaa71fcef57a
Ismael Tondi <smiletondi@gmail.com>
2020-01-02 12:30:04 +0100

    Last commit before submission

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'sp8n4e9i3d'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-3.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'lemag.auth0.com',
+  clientId: 'VzrHEiCFIBKATWkebcnUvHOyfvb9LXxX',
+  callbackUrl: 'http://localhost:3000/callback'
+}
